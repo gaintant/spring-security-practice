4988a04d93984e12634be90dc946d289753c3366
kaushik shah <kaushikshah1309@gmail.com>
2024-12-20 17:16:22 +0530

    added users

---
+++ src/main/java/com/example/security_practice/model/Users.java
@@ -0,0 +1,49 @@
+package com.example.security_practice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    @Id
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name= "Roles")
+    private List<String> roles;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/com/example/security_practice/repository/UserDetails.java
@@ -0,0 +1,2 @@
+package com.example.security_practice.repository;public class UserDetails {
+}
